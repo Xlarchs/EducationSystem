2cf3628e55b97ebfc27396f284030ca24051a9b1
ilkerogtc <ilkerogtc@gmail.com>
2020-09-02 21:20:47 +0300

    Initial commit

---
+++ src/main/java/com/education/education/EducationPostgresqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.education.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPostgresqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationPostgresqlCrudApplication.class, args);
+	}
+
+}
